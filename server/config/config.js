791d662a003880b603194ad3139d2ab406bcb1fe
Omar <omarsalgado37@gmail.com>
2020-11-26 17:25:37 -0600

    Se sube backend para teste telcel

---
+++ server/config/config.js
@@ -0,0 +1,4 @@
+
+//====================
+//Configuracion del puerto una ve que se desplegue
+process.env.PORT = process.env.PORT || 3000;
---
+++ server/server.js
@@ -0,0 +1,32 @@
+require('./config/config')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+let usuarios = [ {nombre:'Omar Salgado', email:'omarsalgado37@gmail.com', 
+                 puesto :'Desarrollador', fecha:'26/11/2020',
+                domicilio:'Venusatinao Carrana CDMX', habilidades:[ 'Cauteloso' ] },
+                 {nombre:'Norma Basaldua', email:'fabi_8@gmail.com', 
+                 puesto :'Desarrollador', fecha:'26/11/2020',
+                domicilio:'Venusatinao Carrana CDMX', habilidades:[ 'Cauteloso' ]},
+                 {nombre:'Paula Alonso', email:'basaldua_34@gmail.com', 
+                 puesto :'Desarrollador', fecha:'26/11/2020',
+                domicilio:'Venusatinao Carrana CDMX', habilidades:[ 'Cauteloso' ]}
+              ]
+app.get('/telcel', function (req, res) {
+  res.json(usuarios)
+})
+app.post('/telcel', function (req, res) {
+
+  let body = req.body;
+  usuarios.push(body);
+  res.json({body});
+})
+ 
+app.listen(process.env.PORT, ()=> {
+    console.log('Estamos escuchando')
+})
